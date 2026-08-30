522f92395b759a0130e616745161798650774535
rollingsmajiwa <rollingsmajiwa@gmail.com>
2026-08-30 21:22:04 +0300

    Feat: create subjects table using sql

--- smart_academic.py
+++ smart_academic.py
@@ -5,7 +5,7 @@ class StudentManager:
         self.db_name = db_name
     def get_connection(self):
         return sqlite3.connect(self.db_name)
-    def create_table(self):
+    def create_table1(self):
         connection = self.get_connection()
         cursor = connection.cursor()
         cursor.execute("""CREATE TABLE IF NOT EXISTS students(student_id INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -14,8 +14,17 @@ class StudentManager:
         cursor.execute("""INSERT INTO students(admission_number, name, grade, stream) VALUES (101, 'Rollings Majiwa', 9, 'N')""")
         connection.commit()
         connection.close()
+    def create_table2(self):
+            connection = self.get_connection()
+            cursor = connection.cursor()
+            cursor.execute("""CREATE TABLE IF NOT EXISTS subjects(subject_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    subject_name TEXT NOT NULL)""")
+            cursor.execute("""INSERT INTO subjects(subject_name) VALUES ('Pre_Technical_studies')""")
+            connection.commit()
+            connection.close()
 
-StudentManager().create_table()
+StudentManager().create_table1()
+StudentManager().create_table2()
 print("Done")
 
 
