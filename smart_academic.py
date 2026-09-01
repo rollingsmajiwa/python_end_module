f861f6893d4d15fc982ef4f1dfb6381b079e0a0e
rollingsmajiwa <rollingsmajiwa@gmail.com>
2026-09-01 23:23:11 +0300

    Fix: fix database

--- smart_academic.py
+++ smart_academic.py
@@ -18,120 +18,131 @@ class StudentManager:
             cursor = connection.cursor()
             cursor.execute("""CREATE TABLE IF NOT EXISTS subjects(subject_id INTEGER PRIMARY KEY AUTOINCREMENT, 
                     subject_name TEXT NOT NULL)""")
+            
             cursor.execute("""INSERT INTO subjects(subject_name) VALUES ('Pre_Technical_studies')""")
+            cursor.execute("""INSERT INTO subjects(subject_name) VALUES ('Mathematics')""")
+            cursor.execute("""INSERT INTO subjects(subject_name) VALUES ('Integrated_science')""")
+            cursor.execute("""INSERT INTO subjects(subject_name) VALUES ('Creative arts')""")
+            cursor.execute("""INSERT INTO subjects(subject_name) VALUES ('Social_studies')""")
+            cursor.execute("""INSERT INTO subjects(subject_name) VALUES ('Agriculture')""")
+            cursor.execute("""INSERT INTO subjects(subject_name) VALUES ('CRE')""")
+            cursor.execute("""INSERT INTO subjects(subject_name) VALUES ('Kiswahili')""")
+            cursor.execute("""INSERT INTO subjects(subject_name) VALUES ('English')""")
+            
+            
             connection.commit()
             connection.close()
-    def create_table3(self):
-                connection = self.get_connection()
-                cursor = connection.cursor()
-                cursor.execute("""CREATE TABLE IF NOT EXISTS marks(subject_id INTEGER, student_id INTEGER,student_mark INTEGER NOT NULL, 
-                term TEXT NOT NULL, year INTEGER NOT NULL)""")
-                cursor.execute("""INSERT INTO marks(subject_id, student_id, student_mark, term, year) VALUES (79, 19, 3, 'TWO', 2026 )""")
+#     def create_table3(self):
+#                 connection = self.get_connection()
+#                 cursor = connection.cursor()
+#                 cursor.execute("""CREATE TABLE IF NOT EXISTS marks(subject_id INTEGER, student_id INTEGER,student_mark INTEGER NOT NULL, 
+#                 term TEXT NOT NULL, year INTEGER NOT NULL)""")
+#                 cursor.execute("""INSERT INTO marks(subject_id, student_id, student_mark, term, year) VALUES (79, 19, 3, 'TWO', 2026 )""")
                 
-                connection.commit()
-                connection.close()
-class StudentInf:
-    def __init__(self, student_id=None, admission_number="", name = "", grade = "10", stream = "N"):
-            self.student_id = student_id
-            self.admission_number = admission_number
-            self.name = name
-            self.grade = grade
-            self.stream = stream
-    def add_student(self):
-          connection = sqlite3.connect("student.db")
-          cursor = connection.cursor()
-          cursor.execute(f"""INSERT INTO students(admission_number, name, grade, stream) VALUES ('{self.admission_number}', '{self.name}', '{self.grade}', '{self.stream}')""")
-          connection.commit()
-          connection.close()
-    def view_student(self):
-          connection = sqlite3.connect("student.db")
-          cursor = connection.cursor()
-          cursor.execute("""SELECT * FROM students""")
-          all_students = cursor.fetchall()
+#                 connection.commit()
+#                 connection.close()
+# class StudentInf:
+#     def __init__(self, student_id=None, admission_number="", name = "", grade = "10", stream = "N"):
+#             self.student_id = student_id
+#             self.admission_number = admission_number
+#             self.name = name
+#             self.grade = grade
+#             self.stream = stream
+#     def add_student(self):
+#           connection = sqlite3.connect("student.db")
+#           cursor = connection.cursor()
+#           cursor.execute(f"""INSERT INTO students(admission_number, name, grade, stream) VALUES ('{self.admission_number}', '{self.name}', '{self.grade}', '{self.stream}')""")
+#           connection.commit()
+#           connection.close()
+#     def view_student(self):
+#           connection = sqlite3.connect("student.db")
+#           cursor = connection.cursor()
+#           cursor.execute("""SELECT * FROM students""")
+#           all_students = cursor.fetchall()
 
-          connection.commit()
-          connection.close()
-          return all_students
-    def delete_student(self):
-            connection = sqlite3.connect("student.db")
-            cursor = connection.cursor()
-            cursor.execute(f"""DELETE FROM students WHERE name = '{self.name}' """)
+#           connection.commit()
+#           connection.close()
+#           return all_students
+#     def delete_student(self):
+#             connection = sqlite3.connect("student.db")
+#             cursor = connection.cursor()
+#             cursor.execute(f"""DELETE FROM students WHERE name = '{self.name}' """)
     
-            connection.commit()
-            connection.close()
-class StudentMarks:
-    def __init__(self, student_id, subject_id, student_mark, term = "Three", year=2027):
-            self.student_id = student_id
-            self.subject_id = subject_id
-            self.student_mark = student_mark
-            self.term = term
-            self.year = year
-    def add_marks(self):
-          connection = sqlite3.connect("student.db")
-          cursor = connection.cursor()
-          cursor.execute(f"""INSERT INTO marks(subject_id, student_id, student_mark, term, year)
-            VALUES ('{self.subject_id}', '{self.student_id}', '{self.student_mark}', '{self.term}', '{self.year}')""")
-          connection.commit()
-          connection.close()
+#             connection.commit()
+#             connection.close()
+# class StudentMarks:
+#     def __init__(self, student_id, subject_id, student_mark, term = "Three", year=2027):
+#             self.student_id = student_id
+#             self.subject_id = subject_id
+#             self.student_mark = student_mark
+#             self.term = term
+#             self.year = year
+#     def add_marks(self):
+#           connection = sqlite3.connect("student.db")
+#           cursor = connection.cursor()
+#           cursor.execute(f"""INSERT INTO marks(subject_id, student_id, student_mark, term, year)
+#             VALUES ('{self.subject_id}', '{self.student_id}', '{self.student_mark}', '{self.term}', '{self.year}')""")
+#           connection.commit()
+#           connection.close()
     
           
         
             
 
-def main():
-      while True:
-            print("\n___STUDENT MANAGEMENT___")
-            print("1. Register student")
-            print("2. View student")
-            print("3. Enter Marks")
-            print("4. Delete student")
-            print("5. Analyze performance")
-            print("6. Generate report")
-            print("7. Exit")
+# def main():
+#       while True:
+#             print("\n___STUDENT MANAGEMENT___")
+#             print("1. Register student")
+#             print("2. View student")
+#             print("3. Enter Marks")
+#             print("4. Delete student")
+#             print("5. Analyze performance")
+#             print("6. Generate report")
+#             print("7. Exit")
 
-            choice = input("Select an option:")
-            if choice == "1":
-                admission_number = input("Enter student admission number:")
-                name = input("Enter student name:")
-                add_st = StudentInf(admission_number=admission_number, name=name)
-                add_st.add_student()
-                print("Student added succesfully")
-            elif choice == "2":
-                viewed_students = StudentInf().view_student()
-                if viewed_students:
+#             choice = input("Select an option:")
+#             if choice == "1":
+#                 admission_number = input("Enter student admission number:")
+#                 name = input("Enter student name:")
+#                 add_st = StudentInf(admission_number=admission_number, name=name)
+#                 add_st.add_student()
+#                 print("Student added succesfully")
+#             elif choice == "2":
+#                 viewed_students = StudentInf().view_student()
+#                 if viewed_students:
 
-                    for data in viewed_students:
-                        print(data)
-                else:
-                      print("Student list is empty!")
+#                     for data in viewed_students:
+#                         print(data)
+#                 else:
+#                       print("Student list is empty!")
             
-            elif choice == "3":
-                student_id = float(input("Enter student id:"))
-                subject_id = float(input("Enter subject id:"))
-                student_mark = float(input("Enter student mark:"))
-                student_m = StudentMarks(student_id, subject_id, student_mark)
-                student_m.add_marks()
-                print("Marks added succesfully")
+#             elif choice == "3":
+#                 student_id = float(input("Enter student id:"))
+#                 subject_id = float(input("Enter subject id:"))
+#                 student_mark = float(input("Enter student mark:"))
+#                 student_m = StudentMarks(student_id, subject_id, student_mark)
+#                 student_m.add_marks()
+#                 print("Marks added succesfully")
 
-            elif choice == "4":
-                  name = input("Enter the full name of the student you want to delete:")
-                  dlete_n = StudentInf(name=name)
-                  dlete_n.delete_student()
-                  print(dlete_n)
-            elif choice == "7":
-                  print("Thank you!")
-                  break
+#             elif choice == "4":
+#                   name = input("Enter the full name of the student you want to delete:")
+#                   dlete_n = StudentInf(name=name)
+#                   dlete_n.delete_student()
+#                   print(dlete_n)
+#             elif choice == "7":
+#                   print("Thank you!")
+#                   break
                 
 
-            else:
-                  print("Invalid choice. Try again")
+#             else:
+#                   print("Invalid choice. Try again")
 
                   
             
             
-main()
+# main()
 
-
+StudentManager().create_table2()
 
 
 
