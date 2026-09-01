ee2f1826540ada755d1bcc37a253b487a0b13c2e
rollingsmajiwa <rollingsmajiwa@gmail.com>
2026-09-01 05:45:52 +0300

    Feat: create add marks function

--- smart_academic.py
+++ smart_academic.py
@@ -24,8 +24,10 @@ class StudentManager:
     def create_table3(self):
                 connection = self.get_connection()
                 cursor = connection.cursor()
-                cursor.execute("""CREATE TABLE IF NOT EXISTS marks(subject_id INTEGER PRIMARY KEY AUTOINCREMENT, student_id INTEGER,student_mark INTEGER NOT NULL, term TEXT NOT NULL, year INTEGER NOT NULL)""")
-                cursor.execute("""INSERT INTO marks(student_id, student_mark, term, year) VALUES (23, 79, 'TWO', 2026 )""")
+                cursor.execute("""CREATE TABLE IF NOT EXISTS marks(subject_id INTEGER, student_id INTEGER,student_mark INTEGER NOT NULL, 
+                term TEXT NOT NULL, year INTEGER NOT NULL)""")
+                cursor.execute("""INSERT INTO marks(subject_id, student_id, student_mark, term, year) VALUES (79, 19, 3, 'TWO', 2026 )""")
+                
                 connection.commit()
                 connection.close()
 class StudentInf:
@@ -50,6 +52,23 @@ class StudentInf:
           connection.commit()
           connection.close()
           return all_students
+class StudentMarks:
+    def __init__(self, student_id, subject_id, student_mark, term = 3, year=2027):
+            self.student_id = student_id
+            self.subject_id = subject_id
+            self.student_mark = student_mark
+            self.term = term
+            self.year = year
+    def add_marks(self):
+          connection = sqlite3.connect("student.db")
+          cursor = connection.cursor()
+          cursor.execute("""INSERT INTO marks(subject_id, student_id, student_mark, term, year)
+            VALUES (?, ?, ?, ?, ?)""", (self.subject_id, self.student_id, self.student_mark, self.term, self.year))
+          connection.commit()
+          connection.close()
+          
+        
+            
 
 def main():
       while True:
@@ -63,16 +82,23 @@ def main():
 
             choice = input("Select an option:")
             if choice == "1":
-                  admission_number = input("Enter student admission number:")
-                  name = input("Enter student name:")
-                  add_st = StudentInf(admission_number=admission_number, name=name)
-                  add_st.add_student()
-                  print("Student added succesfully")
+                admission_number = input("Enter student admission number:")
+                name = input("Enter student name:")
+                add_st = StudentInf(admission_number=admission_number, name=name)
+                add_st.add_student()
+                print("Student added succesfully")
             elif choice == "2":
-                  viewed_students = StudentInf().view_student()
+                viewed_students = StudentInf().view_student()
 
-                  for data in viewed_students:
+                for data in viewed_students:
                         print(data)
+            elif choice == "3":
+                student_id = float(input("Enter student id:"))
+                subject_id = float(input("Enter subject id:"))
+                student_mark = float(input("Enter student mark:"))
+                student_m = StudentMarks(student_id, subject_id, student_mark)
+                student_m.add_marks()
+                print("Marks added succesfully")
 
             else:
                   print("Invalid choice. Try again")
@@ -81,7 +107,9 @@ def main():
             
             
 main()
-    
+
+
+
 
 
 
